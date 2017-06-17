f5c35b480f0a3fbb1eda39a111950136625c5e64
SHIVANI SINGH <shivanisingh6993@gmail.com>
2017-06-18 01:52:23 +0530

    file created

---
+++ backend/server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var path = require('path');
+var sha1 = require('sha1');
+
+
+var MongoClient = require('mongodb').MongoClient
+  
+// Connection url
+var url = 'mongodb://localhost:27017/Examinationdb';
+
+
+
+var app = express();
+
+app.use(express.static(path.resolve(__dirname + './../frontend/views')));
+
+app.use(express.static(path.resolve(__dirname + './../frontend')));
+
+console.log(path.resolve(__dirname + './../frontend'),">>>>>")
+
+
+app.get('/' , function(req,res){
+	res.render('index.html'); 
+})
+
+
+console.log(__dirname);
+
+
+app.post('/SignIn' , function(req,res){
+
+	console.log('>>> data received from front' , req.query);
+	req.query.password = sha1(req.query.password);
+
+MongoClient.connect(url , function(err,db){
+
+if(err){
+	console.log(err);
+}
+console.log('connected')
+
+db.collection('SignIn_tb').insertOne(req.query , function(err,data){
+if(err){
+	return res.send('Error');
+}
+
+setTimeout(function(){
+	res.send('created');
+} , 8000)
+
+
+})
+
+})
+
+})
+app.listen(7000,function(){
+
+
+	console.log('server started');
+});
+
+
---
+++ frontend/controllers/loginController.js
@@ -0,0 +1,26 @@
+angular.module('ExaminationSytem')
+
+.controller('loginController' , function($scope,$http){
+
+$scope.showLoader = false;
+
+
+$scope.login = function(){
+	$scope.showLoader = true;
+	$http({
+		method:'POST',
+		url:'/SignIn',
+		params:{
+			name : $scope.name,
+			password : $scope.pass
+		}
+	})
+	.then(function(response){
+     $scope.showLoader = false;
+	})
+}
+
+
+
+
+})
